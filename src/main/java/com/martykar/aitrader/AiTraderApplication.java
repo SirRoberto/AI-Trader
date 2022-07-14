f2f8bf354e3e368209e7ed775c73a50e09cab5f5
Robert Martyka <r.martyka@sages.com.pl>
2022-07-15 01:41:35 +0200

    Initial commit

---
+++ src/main/java/com/martykar/aitrader/AiTraderApplication.java
@@ -0,0 +1,13 @@
+package com.martykar.aitrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTraderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiTraderApplication.class, args);
+    }
+
+}
